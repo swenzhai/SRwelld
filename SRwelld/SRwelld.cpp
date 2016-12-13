b1e8950de8bc1393170c4b2ec0c1008596a30fb1
swenz <swenz@DESKTOP-BRE4E8V>
2016-12-13 19:36:49 +0800

    添加项目文件。

---
+++ SRwelld/SRwelld.cpp
@@ -0,0 +1,87 @@
+// SRwelld.cpp : 定义 DLL 应用程序的导出函数。
+//
+#define SRWELL_EXPORTS
+#include "stdafx.h"
+#include "SRwelld.h"
+
+//#include "include\imgproc.hpp"
+#include "include\imgproc_c.h"
+#pragma comment(lib,"lib\\zlib.lib")
+#pragma comment(lib,"lib\\IlmImf.lib")
+#pragma comment(lib,"lib\\libtiff.lib")
+#pragma comment(lib,"lib\\libjasper.lib")
+#pragma comment(lib,"lib\\opencv_core2413.lib")
+#pragma comment(lib,"lib\\opencv_imgproc2413.lib")
+
+// 这是导出变量的一个示例
+//SRWELLD_API int nSRwelld=0;
+// 这是导出函数的一个示例。
+//SRWELLD_API int fnSRwelld(void)
+//{
+//    return 42;
+//}
+// 这是已导出类的构造函数。
+// 有关类定义的信息，请参阅 SRwelld.h
+//CSRwelld::CSRwelld()
+//{
+//    return;
+//}
+//
+
+IplImage *src = 0;
+IplImage *dst = 0;
+
+SRWELLD_API int __stdcall srCreateResizeCoef(BYTE * pSrc, BITMAPINFO * pBmpInfo, BYTE ** pDst, double scale)
+{
+	if (!pSrc || !pBmpInfo || !pDst)
+		return -1;
+	if (scale < 0 || scale >10)
+		return -2;
+	if (pBmpInfo->bmiHeader.biWidth <= 0 || pBmpInfo->bmiHeader.biHeight <= 0 || pBmpInfo->bmiHeader.biBitCount <= 0 || pBmpInfo->bmiHeader.biPlanes <= 0)
+		return -3;
+	if (src || dst)
+		return -4;
+
+	CvSize dst_cvsize;
+	src = cvCreateImageHeader(cvSize(pBmpInfo->bmiHeader.biWidth, pBmpInfo->bmiHeader.biHeight),
+		pBmpInfo->bmiHeader.biBitCount, pBmpInfo->bmiHeader.biPlanes);
+	
+	//widthStep 4字节对齐
+	int widthStep = (4 - (pBmpInfo->bmiHeader.biWidth*pBmpInfo->bmiHeader.biPlanes-1) % 4) 
+		+ pBmpInfo->bmiHeader.biWidth*pBmpInfo->bmiHeader.biPlanes;
+	cvSetData(src, pSrc, widthStep);
+
+	dst_cvsize.width = (int)(src->width*scale);
+	dst_cvsize.height = (int)(src->height*scale);
+	dst = cvCreateImage(dst_cvsize, src->depth, src->nChannels);
+	*pDst = (BYTE*)dst->imageDataOrigin;
+
+	return 0;
+}
+
+SRWELLD_API int __stdcall srResize(int method)
+{
+	if (method < 0 || method>4)
+		return -1;
+	if (!src || !dst)
+		return -2;
+	cvResize(src, dst, method);
+	return  0;
+}
+
+SRWELLD_API int __stdcall srReleaseResizeCoef()
+{
+	//release buffer
+	if (!src || !dst)
+		return -1;
+	cvReleaseImageHeader(&src);
+	cvReleaseImage(&dst);
+	src = dst = 0;
+	return  0;
+}
+
+SRWELLD_API int __stdcall srAbout()
+{
+	MessageBox(NULL, TEXT("Copy Right: Shenzhen Welld CO,.Ltd\n SR Module.\n Version 1.0"),TEXT("About"), MB_OK);
+	return 0;
+}
